be0725c2d2277eecf38e7e8aaa74997d9cd77394
Abdelatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-02-28 10:13:56 +0100

    update

---
+++ login.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/login.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.7.1/chart.min.js"></script>
+    <title>Login</title>
+</head>
+<body>
+    <header>
+        <a href="" class="logo"><span>Tex</span>GEAR<span></span></a>
+    </header>
+
+    <div class="container">
+        <h1>IDETIFICATION</h1>
+        <hr color="#FF304F" width="250" size="4">
+
+        <form action="login.php">
+            <div class="input-container">
+                <i class="icon"><img src="res/user.png" alt=""></i>
+                <input class="input-field" type="text" placeholder="Username" name="usrnm">
+            </div>
+
+            <div class="input-container">
+                <i class="icon"><img src="res/password.png" alt=""></i>
+                <input class="input-field" type="password" placeholder="Password" name="psw">
+            </div>
+
+            <button type="submit" class="btn">LOGIN</button>
+
+            <a href="reset_password.php">Forgot password ?</a>
+        </form> 
+    </div>
+
+    <footer>
+        <center>
+            <p>Tex-GEAR, All rights reserved @2022</p>
+        </center>
+    </footer>
+</body>
+</html>
