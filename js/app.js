b8f5d362491a4f378c2e04fbd81bb8ca4a9a8808
Abdelatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-02-28 19:02:30 +0100

    full setup

--- js/app.js
+++ js/app.js
@@ -1,3 +1,17 @@
 let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']; 
 
 let data = []; // the data of each month
+
+var target = document.getElementsByClassName(".nav-link");
+var mainContent = document.querySelector("main");
+
+target.addEventListener("mouseover", mOver, false);
+target.addEventListener("mouseout", mOut, false);
+
+function mOver() {
+   mainContent.classList.add(".active");
+}
+
+function mOut() {  
+   mainContent.classList.remove(".active");
+}
---
+++ pages/dashboard.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style/dashboard.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.7.1/chart.min.js"></script>
+    <script src="../js/app.js"></script>
+    <title>Admin Page</title>
+</head>
+<body>
+    <nav class="navbar">
+        <ul class="navbar-nav">
+            <li class="logo">
+                <a href="#" class="nav-link">
+                <span class="link-text logo-text">Tex<font color="#fff">GEAR</font></span>
+                <img src="../res/img/logo.svg" alt="">
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                    <img class="nav-icon" src="../res/img/chart.png" alt="">
+                    <span class="link-text">Dashboard</span>
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                    <img class="nav-icon" src="../res/img/profile1.png" alt="">
+                    <span class="link-text">Profile</span>
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                    <img class="nav-icon" src="../res/img/categories.png" alt="">
+                    <span class="link-text">Category</span>
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                    <img class="nav-icon" src="../res/img/product.png" alt="">
+                    <span class="link-text">Products</span>
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                    <img class="nav-icon" src="../res/img/reports.png" alt="">
+                    <span class="link-text">Reports</span>
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                    <img class="nav-icon" src="../res/img/logout.png" alt="">
+                    <span class="link-text">Logout</span>
+                </a>
+            </li>
+        </ul>
+    </nav>
+
+    <!-- MAIN CONTENT -->
+    <main>
+        <h1>Main content</h1>
+    </main>
+</body>
+</html>
